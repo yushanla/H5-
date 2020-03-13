f59141a4206a5e7c6011cbf741e43d0c21f7a596
iyushan19941102 <yushanla@outlook.com>
2020-03-13 19:03:41 +0800

    page_update

--- js/index.js
+++ js/index.js
@@ -43,13 +43,7 @@ function creatcell() {
     return temp;
 }
 
-//删除某行
-function delrow() {
-    var con = $('con');
-    if (con.childNodes.length == 6) {
-        con.removeChild(con.lastChild);
-    }
-}
+
 
 //点击开始游戏按钮 开始游戏
 function start() {
@@ -71,7 +65,7 @@ function init() {
 
     // 添加onclick事件
     $('main').onclick = function(ev) {
-        ev = ev || event
+        ev = ev || event //浏览器 兼容
         judge(ev);
     }
 
@@ -82,13 +76,14 @@ function init() {
 
 // 判断是否点击黑块、白块
 function judge(ev) {
+    console.log(ev.target.className);
     if (ev.target.className.indexOf('black') == -1 && ev.target.className.indexOf('cell') !== -1) {
-        ev.target.parentNode.pass1 = 1; //定义属性pass，表示此行row的白块已经被点击
+        ev.target.parentNode.pass_white = 1; //定义属性pass_white，表示此行row的白块已经被点击
     }
 
     if (ev.target.className.indexOf('black') !== -1) { //点击目标元素 类名中包含 black 说明是黑块
         ev.target.className = 'cell';
-        ev.target.parentNode.pass = 1; //定义属性pass，表明此行row的黑块已经被点击
+        ev.target.parentNode.pass_black = 1; //定义属性pass_black，表明此行row的黑块已经被点击
         score();
     }
 
@@ -96,15 +91,15 @@ function judge(ev) {
 }
 
 // 判断游戏是否结束
-
 function over() {
     var rows = con.childNodes;
-    console.log(rows);
-    if ((rows.length == 5) && (rows[rows.length - 1].pass !== 1)) {
+    //黑块触底 --> 失败
+    if ((rows.length == 5) && (rows[rows.length - 1].pass_black !== 1)) {
         fail();
     }
+    //点击白块 --> 失败
     for (let i = 0; i < rows.length; i++) {
-        if (rows[i].pass1 == 1) {
+        if (rows[i].pass_white == 1) {
             fail();
         }
     }
@@ -115,13 +110,12 @@ function fail() {
     clearInterval(clock);
     flag = false;
     var score = $('score').innerHTML;
-    saveScore(score);
     confirm('你的最终得分为 ' + parseInt(score));
+    saveScore(score);
     var con = $('con');
     con.innerHTML = "";
     $('score').innerHTML = 0;
     con.style.top = '-408px';
-
 }
 
 //让黑块动起来
@@ -143,7 +137,13 @@ function move() {
     }
 }
 
-
+//删除某行
+function delrow() {
+    var con = $('con');
+    if (con.childNodes.length == 6) {
+        con.removeChild(con.lastChild);
+    }
+}
 // 加速函数
 function speedup() {
     speed += 2;
@@ -165,14 +165,15 @@ function score() {
 
 //记录存储
 function saveScore(score) {
-    if (sessionStorage.high != undefined) {
+    if (sessionStorage.high == undefined) {
+        sessionStorage.high = score;
+        $('highscore').innerHTML = score;
+
+    } else {
         var highscore = sessionStorage.high;
         if (highscore < score) {
             highscore = score;
             $('highscore').innerHTML = highscore;
         }
-    } else {
-        sessionStorage.high = score;
-        $('highscore').innerHTML = score;
     }
 }
