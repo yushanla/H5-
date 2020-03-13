5b1238938cf833c5146f1845a54446467a41365e
iyushan19941102 <yushanla@outlook.com>
2020-03-13 11:39:55 +0800

    explore

--- js/index.js
+++ js/index.js
@@ -15,13 +15,48 @@ var state = 0;
 var speed = 6;
 var flag = false;
 
+// 创造一个<div class="row">并且有四个子节点<div class="cell">
+function createrow() {
+    var con = $('con');
+    var row = creatediv('row'); //创建div className=row
+    var arr = creatcell(); //定义div cell的类名,其中一个为cell black
+
+    con.appendChild(row); // 添加row为con的子节点
+
+    for (var i = 0; i < 4; i++) {
+        row.appendChild(creatediv(arr[i])); //添加row的子节点 cell
+    }
+
+    if (con.firstChild == null) {
+        con.appendChild(row);
+    } else {
+        con.insertBefore(row, con.firstChild);
+    }
+}
+
+
+// 创建一个类名的数组，其中一个为cell black, 其余为cell
+function creatcell() {
+    var temp = ['cell', 'cell', 'cell', 'cell', ];
+    var i = Math.floor(Math.random() * 4); //随机产生黑块的位置 Math.random()函数参数 0~1的随机数
+    temp[i] = 'cell black';
+    return temp;
+}
+
+//删除某行
+function delrow() {
+    var con = $('con');
+    if (con.childNodes.length == 6) {
+        con.removeChild(con.lastChild);
+    }
+}
 
 //点击开始游戏按钮 开始游戏
 function start() {
     if (!flag) {
         init();
     } else {
-        alert('游戏已经开始，无须再次点击！')
+        //alert('游戏已经开始，无须再次点击！')
     }
 }
 
@@ -64,6 +99,7 @@ function judge(ev) {
 
 function over() {
     var rows = con.childNodes;
+    console.log(rows);
     if ((rows.length == 5) && (rows[rows.length - 1].pass !== 1)) {
         fail();
     }
@@ -72,7 +108,6 @@ function over() {
             fail();
         }
     }
-
 }
 
 // 游戏结束
@@ -87,34 +122,6 @@ function fail() {
     $('score').innerHTML = 0;
     con.style.top = '-408px';
 
-}
-
-// 创造一个<div class="row">并且有四个子节点<div class="cell">
-function createrow() {
-    var con = $('con');
-    var row = creatediv('row'); //创建div className=row
-    var arr = creatcell(); //定义div cell的类名,其中一个为cell black
-
-    con.appendChild(row); // 添加row为con的子节点
-
-    for (var i = 0; i < 4; i++) {
-        row.appendChild(creatediv(arr[i])); //添加row的子节点 cell
-    }
-
-    if (con.firstChild == null) {
-        con.appendChild(row);
-    } else {
-        con.insertBefore(row, con.firstChild);
-    }
-}
-
-
-// 创建一个类名的数组，其中一个为cell black, 其余为cell
-function creatcell() {
-    var temp = ['cell', 'cell', 'cell', 'cell', ];
-    var i = Math.floor(Math.random() * 4); //随机产生黑块的位置 Math.random()函数参数 0~1的随机数
-    temp[i] = 'cell black';
-    return temp;
 }
 
 //让黑块动起来
@@ -145,13 +152,7 @@ function speedup() {
     }
 }
 
-//删除某行
-function delrow() {
-    var con = $('con');
-    if (con.childNodes.length == 6) {
-        con.removeChild(con.lastChild);
-    }
-}
+
 
 // 记分
 function score() {
