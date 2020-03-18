31702bf04ed7a741165d74fc3ac954bc924aea1c
iyushan19941102 <yushanla@outlook.com>
2020-03-18 18:04:04 +0800

    resize

--- js/index.js
+++ js/index.js
@@ -1,186 +1,159 @@
-// 工具封装
-// 根据id来获取元素
-function $(id) {
-    return document.getElementById(id);
+//获取元素
+function $(ele) {
+    return document.getElementById(ele);
 }
 
-// 创建div, className是其类名
-function creatediv(className) {
-    var div = document.createElement('div');
-    div.className = className;
-    return div;
-}
 var clock = null;
-var state = 0;
-var speed = 6;
 var flag = false;
+var speed = 6;
 
-// 创造一个<div class="row">并且有四个子节点<div class="cell">
-function createrow() {
-    var con = $('con');
-    var row = creatediv('row'); //创建div className=row
-    var arr = creatcell(); //定义div cell的类名,其中一个为cell black
-    con.appendChild(row); // 添加row为con的子节点
-
-    for (var i = 0; i < 4; i++) {
-        row.appendChild(creatediv(arr[i])); //添加row的子节点 cell
-    }
-
-    if (con.firstChild == null) {
-        con.appendChild(row);
-    } else {
-        con.insertBefore(row, con.firstChild);
-    }
-}
-
-
-// 创建一个类名的数组，其中一个为cell black, 其余为cell
-function creatcell() {
-    var temp = ['cell', 'cell', 'cell', 'cell', ];
-    var i = Math.floor(Math.random() * 4); //随机产生黑块的位置 Math.random()函数参数 0~1的随机数
-    temp[i] = 'cell black';
-    return temp;
-}
-
-
-
-//点击开始游戏按钮 开始游戏
+//
 function start() {
     if (!flag) {
+        flag = true;
         init();
-    } else {
-        //alert('游戏已经开始，无须再次点击！')
     }
 }
 
-/*
- *    初始化 init
- */
+//初始化
 function init() {
-    flag = true;
     for (var i = 0; i < 4; i++) {
-        createrow();
+        createRow();
     }
-
-    // 添加onclick事件
     $('main').onclick = function(ev) {
-        ev = ev || event //浏览器 兼容
-        judge(ev);
+        ev = ev || event;
+        jduge(ev);
     }
 
-    // 定时器 每30毫秒调用一次move()
     clock = window.setInterval('move()', 30);
 }
 
-
-// 判断是否点击黑块、白块
-function judge(ev) {
-    if (ev.target.className.indexOf('black') == -1 && ev.target.className.indexOf('cell') !== -1) {
-        ev.target.parentNode.pass_white = 1; //定义属性pass_white，表示此行row的白块已经被点击
-        over();
+//创建行
+function createRow() {
+    var con = $('con');
+    var row = createDiv('row');
+    var cell = createCell();
+    for (var j = 0; j < 4; j++) {
+        row.appendChild(cell[j]);
     }
-
-    if (ev.target.className.indexOf('black') !== -1) { //点击目标元素 类名中包含 black 说明是黑块
-        ev.target.className = 'cell';
-        ev.target.parentNode.pass_black = 1; //定义属性pass_black，表明此行row的黑块已经被点击
-        score();
+    if (con.firstChild == null) {
+        con.appendChild(row); //init()
+    } else {
+        con.insertBefore(row, con.firstChild); //move()
     }
-
-
 }
 
-// 判断游戏是否结束
+//创建格子
+function createCell() {
+    var arr = new Array;
+    for (var i = 0; i < 4; i++) {
+        arr[i] = createDiv('cell');
+    }
+    var balck = Math.floor(Math.random() * 4);
+    arr[balck].setAttribute('class', 'cell black');
+    return arr;
+}
+
+//创建div
+function createDiv(attr) {
+    var newDiv = document.createElement('div');
+    newDiv.setAttribute('class', attr);
+    return newDiv;
+}
+
+//删除行
+function delRow() {
+    var con = $('con');
+    //当行数=6时才删行
+    if (con.childNodes.length == 6) {
+        con.removeChild(con.lastChild);
+    }
+    //debugger;
+}
+//判断点击块颜色
+function jduge(ev) {
+    //如果不是黑色，over()
+    if (ev.target.className.indexOf('black') == -1) {
+        ev.target.parentNode.pass_white = 1;
+        over();
+    }
+    if (ev.target.className.indexOf('black') !== -1) {
+        ev.target.className = 'cell';
+        ev.target.parentNode.pass_black = 1;
+        score();
+    }
+}
+//成绩记录
+function score() {
+    var scorenum = parseInt($('score').innerHTML);
+    scorenum += 1;
+    $('score').innerHTML = scorenum;
+    if (scorenum % 10 == 0) {
+        speed += 2;
+        win();
+    }
+}
+//奖励图像
+function win() {
+    $('img0').style.opacity = 1;
+    setInterval("$('img0').style.opacity = 0", 2500);
+}
+//最高成绩记录
+function highscore(scorenum) {
+    var num = parseInt(sessionStorage.high);
+    if (!isNaN(num)) {
+        if (num < scorenum) {
+            num = scorenum;
+        }
+    } else {
+        num = 0;
+        num = scorenum;
+    }
+    sessionStorage.high = num;
+    $('highscore').innerHTML = num;
+}
+//移动
+function move() {
+    var con = $('con');
+    var top = parseInt(window.getComputedStyle(con, null)['top']);
+    if (top + speed > 0) {
+        top = 0;
+    } else {
+        top += speed;
+    }
+    con.style.top = top + 'px';
+    over();
+    //debugger;
+    if (top == 0) {
+        createRow();
+        con.style.top = '-102px';
+        delRow();
+    }
+}
+
 function over() {
-    var rows = con.childNodes;
-    //黑块触底 --> 失败
-    if ((rows.length == 5) && (rows[rows.length - 1].pass_black !== 1)) {
+    var row = $('con').childNodes;
+    //黑块触底
+    if ((row[row.length - 1].pass_black !== 1) && (row.length == 5)) {
         fail();
     }
-    //点击白块 --> 失败
-    for (let i = 0; i < rows.length; i++) {
-        if (rows[i].pass_white == 1) {
+    //点击白块
+    for (let i = 0; i < row.length; i++) {
+        if (row[i].pass_white == 1) {
             fail();
         }
     }
 }
 
-// 游戏结束
 function fail() {
     clearInterval(clock);
-    flag = false;
-    speed = 6;
-    var score = $('score').innerHTML;
-    confirm('你的最终得分为 ' + parseInt(score));
-    saveScore(score);
+    var scorenum = $('score').innerHTML;
+    alert('最终成绩： ' + scorenum);
     var con = $('con');
-    con.innerHTML = "";
-    $('score').innerHTML = 0;
     con.style.top = '-408px';
-}
-
-//让黑块动起来
-function move() {
-    var con = $('con');
-    var top = parseInt(window.getComputedStyle(con, null)['top']);
-
-    if (speed + top > 0) {
-        top = 0;
-    } else {
-        top += speed;
-    }
-    con.style.top = top + 'px'; //不断移动top值，使它动起来
-    over();
-    if (top == 0) {
-        createrow();
-        con.style.top = '-102px';
-        delrow();
-    }
-}
-
-//删除某行
-function delrow() {
-    var con = $('con');
-    if (con.childNodes.length == 6) {
-        con.removeChild(con.lastChild);
-    }
-}
-// 加速函数
-function speedup() {
-    speed += 2;
-    if (speed == 20) {
-        win();
-    }
-}
-
-//浮现一只比V黄熊，然后隐去
-function win() {
-    var img = document.getElementById('img0');
-    img.style.opacity = 0.7;
-    setTimeout(() => {
-        img.style.opacity = 0;
-    }, 2000);
-}
-
-
-// 记分
-function score() {
-    var newscore = parseInt($('score').innerHTML) + 1; //分数加一
-    $('score').innerHTML = newscore; //修改分数
-    if (newscore % 10 == 0) { //当分数是10 的倍数时使用加速函数，越来越快
-        speedup();
-    }
-}
-
-//记录存储
-function saveScore(score) {
-    if (sessionStorage.high == undefined) {
-        sessionStorage.high = score;
-        $('highscore').innerHTML = score;
-    } else {
-        var highscore = sessionStorage.high;
-        if (highscore < score) {
-            highscore = score;
-            $('highscore').innerHTML = highscore;
-        }
-    }
+    flag = false;
+    highscore(scorenum);
+    speed = 6;
+    con.innerHTML = '';
+    $('score').innerHTML = 0;
 }
